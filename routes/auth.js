9f8c904bb98117893b3b3208bcfb6e8caf0f50e2
Songkyeok <kihyeoksong0730@gmail.com>
2024-06-18 17:24:21 +0900

    카카오 로그인 처리 추가

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
+const db = require('../db.js');
+const sql = require('../sql.js');
 
-// 소셜 회원가입
+
+// 카카오 회원가입
+
+
+// 카카오 로그인
+router.post('/kakaoLogin', (req, res) => {
+    const kakao = req.body;
+
+    // 데이터 없을 시 회원가입 진행
+    db.query(sql.kakaoLogin, [kakao.user_id], (err, results) => {
+        if(results.length <= 0) {
+            db.query(sql.kakaoJoin, [kakao.user_id, kakao.user_nm, kakao.user_id], err => {
+                if(err) {
+                    console.error(err);
+                    res.status(500).json({
+                        error: 'error'
+                    })
+                }
+            })
+        }
+
+        // 로그인
+        db.query(sql.getUserNo, [kakao.user_id], (err, results) => {
+            if (err) {
+                console.error(err);
+            }
+            res.status(200).json({
+                message: results[0].user_no
+            })
+        })
+    })
+})
 
 module.exports = router;
---
+++ sql.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // auth
+    getUserNo: `SELECT user_no FROM tb_user WHERE user_id = ?`,
+
+    // 카카오
+    kakaoLogin: `SELECT * FROM tb_user WHERE user_id = ?`,
+    kakaoJoin: `INSERT INTO tb_user (user_id, user_nm, user_email) VALUES(?,?,?)`,
+}
