73276160f0949af43d8f2401421201883cc98b80
areauming <djarys92@gmail.com>
2024-06-20 17:25:59 +0900

    회원가입

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,7 @@ router.post('/join', function(req, res) {
     const user = req.body;
     const encryptedPW = bcrypt.hashSync(user.user_pw, 10); // 비밀번호 암호화
 
-    db.query(sql.checkUser_Id, [user.user_id], function (error, results, fields) {
+    db.query(sql.checkUserId, [user.user_id], function (error, results, fields) {
         if (results.length <= 0) {
             db.query(sql.join, [user.user_id, user.user_email, encryptedPW, user.user_nm, user.user_phone, user.user_zipcode, user.user_adr1, user.user_adr2],
                 function (error, data) {
--- sql.js
+++ sql.js
@@ -9,4 +9,10 @@ module.exports = {
     // 네이버
     naverLogin: `SELECT * FROM tb_user WHERE user_id = ?`,
     naverJoin: `INSERT INTO tb_user (user_id, user_nm, user_email) VALUES(?,?,?)`,
+
+    // 로컬 회원가입
+    join: `INSERT INTO tb_user (user_id, user_nm, user_email, user_pw, user_phone, user_zipcode, user_adr1, user_adr2) VALUES(?,?,?,?,?,?,?,?)`,
+    id_check: `SELECT * FROM tb_user WHERE user_id = ?`,
+    login: `SELECT user_pw FROM tb_user WHERE user_id = ?`,
+
 }
