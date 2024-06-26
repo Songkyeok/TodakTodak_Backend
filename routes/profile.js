d8c39c8dd8b7b9feb8ff267ca9a4982e42d0ef11
Songkyeok <kihyeoksong0730@gmail.com>
2024-06-26 16:05:59 +0900

    회원정보 수정 추가

--- routes/profile.js
+++ routes/profile.js
@@ -6,14 +6,27 @@ const bcrypt = require('bcrypt');
 router.post('/selectProfile', (req, res) => {
     const user_no = req.body.user_no;
 
-    console.log("user_no ===>> ", user_no)
     db.query(sql.selectProfile, [user_no], (err, data) => {
         if(err) {
             return res.status(500).json({ error: err });
         }
-        console.log("data ===>", data[0])
         res.json(data[0]);
     })
+});
+
+router.post("/updateProfile", (req, res) => {
+    const data = req.body;
+    
+    db.query(sql.updateProfile, [data.user_email, data.user_phone, data.user_zipcode, data.user_adr1, data.user_adr2, data.user_id], (err, data) => {
+        if(err) {
+            return res.status(500).json({ error: err })
+        }
+
+        console.log(data);
+        res.json(data);
+    })
+
+    console.log(data);
 })
 
 module.exports = router;
